206877a3efb4a8ef715242904dc1967d6d8cdce9
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-09-05 15:21:24 -0300

    Implementa funções para API do mercado livre.

---
+++ src/services/productsApi.js
@@ -0,0 +1,13 @@
+async function getProducts(categoryId = '', query = '') {
+  try {
+    const END_POINT = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const response = await fetch(END_POINT);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
+}
+
+export default getProducts;
