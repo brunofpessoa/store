6a17777204c8ee84a1bb134f4f4fbe1c9d4f27bc
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-09-06 11:22:20 -0300

    Install React Router Dom v6.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
   );
 }
 
